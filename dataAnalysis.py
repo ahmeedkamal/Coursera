faec6cd4abcbf565851abf9a764ef6943df5c16f
Ahmed Kamal <ahmedkamal1410@gmail.com>
2015-10-26 02:09:36 +0100

    Week 4

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -6,6 +6,8 @@ data analysis specialization
 # imports
 import pandas
 import numpy
+import seaborn
+import matplotlib.pyplot as pyplot
 
 # data extraction
 aData = pandas.read_csv('ool_pds.csv')
@@ -25,7 +27,7 @@ aData = pandas.read_csv('ool_pds.csv')
 #       "================================")
 
 ##select subdata for people 18-25 years old
-#aSubData = aData[(aData['PPAGE'] <= 25) & (aData['PPAGE'] >= 18)]
+aSubData = aData[(aData['PPAGE'] <= 25) & (aData['PPAGE'] >= 18)]
 #
 #print "Histogram of ages:"
 #ageHisto = aSubData.groupby("PPAGE").size()
@@ -101,12 +103,16 @@ aData = pandas.read_csv('ool_pds.csv')
 aSubData = aData[(aData['PPAGE'] <= 25) & (aData['PPAGE'] >= 18)]
 
 # Replace missing data (-1 represents "Refused") with NaN
-aSubData['W1_C1'] = aSubData['W1_C1'].replace(-1, numpy.nan)
+print aSubData['W1_C1'].value_counts(sort=False, dropna=False)
+#aSubData['W1_C1'] = aSubData['W1_C1'].replace(-1, numpy.nan)
+aSubData = aSubData[aSubData['W1_C1'] != -1]
 print "Histogram of political choices after replacing missing data:"
 print aSubData['W1_C1'].value_counts(sort=False, dropna=False)
  
 
 # Replace missing data in IDs with a valid number not already used
+
+print aSubData['W2_CASEID2'].value_counts(sort=True, dropna=False)
 newIDs = list()
 def validID(row):
     num = 2
@@ -122,10 +128,13 @@ aSubData['W2_CASEID2'] = aSubData.apply(lambda row: validID(row), axis=1)
 
 print "Histogram of W1_C2 after filling missing data with valid data:"
 print aSubData['W2_CASEID2'].value_counts(sort=True, dropna=False)
-
+print "#######################"
+print aSubData['W1_C1B'].value_counts(sort=True, dropna=False)
 # Replace missing data (-1  and .) with NaN
 aSubData['W1_C1B'] = aSubData['W1_C1B'].replace(-1, numpy.nan)
 aSubData['W1_C1B'] = aSubData['W1_C1B'].replace('.', numpy.nan)
+aSubData = aSubData[aSubData['W1_C1B'] != numpy.nan]
+
 print "Histogram of W1_C1B after coding in valid data:"
 print aSubData['W1_C1B'].value_counts(sort=True, dropna=False)
 
@@ -134,8 +143,30 @@ print aSubData['W1_C1B'].value_counts(sort=True, dropna=False)
 # We will also replace missing data with NaN
 
 print aSubData['W1_C2'].value_counts(sort=True, dropna=False)
-regroup = {1:1, 2:1, 3:1, 4:2, 5:3, 6:3, 7:3, -1:numpy.nan}
+regroup = {1:1, 2:1, 3:1, 4:2, 5:3, 6:3, 7:3}#, -1:numpy.nan}
 aSubData['W1_C2'] = aSubData['W1_C2'].map(regroup)
+#aSubData = aSubData[aSubData['W1_C2'] != -1]
 
 print "Histogram of W1_C2 after grouping:"
 print aSubData['W1_C2'].value_counts(sort=True, dropna=False)
+
+###################################################
+##################### WEEK 4 ######################
+###################################################
+
+aSubData['W1_C1B'] = aSubData['W1_C1B'].convert_objects(convert_numeric=True)
+aSubData['W1_C1'] = aSubData['W1_C1'].convert_objects(convert_numeric=True)
+aSubData['PPAGE'] = aSubData['PPAGE'].convert_objects(convert_numeric=True)
+aSubData['W1_C2'] = aSubData['W1_C2'].convert_objects(convert_numeric=True)
+
+#Display a participants age plot
+pyplot.xlabel('Political choices of participants')
+pyplot.ylabel('# of participants')
+seaborn.distplot(aSubData['W1_C2'], kde=False)
+
+Display a political choices plot for participants (12-25)
+pyplot.xlabel('Political choices')
+pyplot.ylabel('# of participants aged 18-25')
+seaborn.distplot(aSubData['W1_C1'], kde=False)
+
+seaborn.factorplot(x='W1_C1', y='PPAGE', data=aSubData, kind='bar', ci=None)
